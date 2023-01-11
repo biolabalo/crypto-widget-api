5486cf3e9d88890a268e5de5fe2972876375de05
balogun biola <balogunbiola101@gmail.com>
2023-01-11 20:33:40 +0100

    chore: ensure latest node version is used

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Make sure we are running node 19.4.0
+  const latestNodeVersion = '19.4.0';
+  if (process.versions.node !== latestNodeVersion) {
+    console.log('Running the app requires node 19.0.4');
+    process.exit();
+  }
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
